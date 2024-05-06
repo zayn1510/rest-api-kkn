e04e638c61f91bf47c051dd89e7c99116b305418
zayn1510 <zayndevs.1510@gmail.com>
2024-05-06 20:09:16 +0800

    update v2

--- src/test/java/com/api/kkn/app/ApiKknApplicationTests.java
+++ src/test/java/com/api/kkn/app/ApiKknApplicationTests.java
@@ -2,12 +2,10 @@ package com.api.kkn.app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
 @SpringBootTest
 class ApiKknApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
